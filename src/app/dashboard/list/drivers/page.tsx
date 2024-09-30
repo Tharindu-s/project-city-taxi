b2fd48a6e00cdf98029e784398a4c2b14a9ea21b
Vimukthi Madusankha <xpercube@gmail.com>
2024-09-30 23:20:19 +0530

    Map

--- src/app/dashboard/list/drivers/page.tsx
+++ src/app/dashboard/list/drivers/page.tsx
@@ -1,17 +1,6 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import prisma from "@/lib/db";
-import Link from "next/link";
-import { VscVerifiedFilled } from "react-icons/vsc";
-import { MdError } from "react-icons/md";
+import UsersTable from "@/components/common/UsersTable";
+import React from "react";
 
 export default async function TableDemo() {
   const guests = await prisma.guest.findMany({
@@ -30,43 +19,8 @@ export default async function TableDemo() {
   });
 
   return (
-    <div className="m-6">
-      <Table className="p-6">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">ID</TableHead>
-            <TableHead>Name</TableHead>
-            {/*  <TableHead>Method</TableHead> */}
-            <TableHead className="text-right">Verified status</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {guests.map((guest) => (
-            <TableRow key={guest.id}>
-              <TableCell className="font-medium">
-                <Link href={`users/${guest.id}`}>{guest.id} </Link>
-              </TableCell>
-              <TableCell>
-                <Link href={`users/${guest.id}`}>{guest.name} </Link>
-              </TableCell>
-              {/*<TableCell>{guest.phone}</TableCell> */}
-              <TableCell className="text-right flex justify-end items-center">
-                {guest?.isVerified === false ? (
-                  <MdError
-                    size={24}
-                    style={{ color: "red", marginRight: "8px" }}
-                  />
-                ) : (
-                  <VscVerifiedFilled
-                    size={24}
-                    style={{ color: "green", marginRight: "8px" }}
-                  />
-                )}
-              </TableCell>{" "}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+    <>
+      <UsersTable users={guests} usertype="drivers" />
+    </>
   );
 }
--- src/app/dashboard/list/passengers/[id]/page.tsx
+++ src/app/dashboard/list/passengers/[id]/page.tsx
@@ -1,13 +1,9 @@
-import { createUser, editGuest } from "@/actions/actions";
-import GuestDelete from "@/components/admin/forms/GuestDelete";
-import Layout from "@/components/layout/Layout";
 import prisma from "@/lib/db";
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 import AddUser from "@/components/forms/AddUserForm";
 import { VscVerifiedFilled } from "react-icons/vsc";
 import { MdError } from "react-icons/md";
+import UserInfo from "@/components/common/UserInfo";
 
 interface GuestParams {
   id: number;
@@ -15,7 +11,6 @@ interface GuestParams {
 
 export default async function guest({ params }: { params: GuestParams }) {
   // get one guest
-
   const guest = await prisma.guest.findUnique({
     where: {
       // dynamic params are strings. convert to a number
@@ -23,117 +18,5 @@ export default async function guest({ params }: { params: GuestParams }) {
     },
   });
 
-  return (
-    <>
-      <div>
-        <section className="contact-section fix mt-12">
-          <div className="container">
-            <div className="contact-wrapper-2">
-              <div className="row g-4 ">
-                {/* guest details */}
-
-                <div className="col-lg-6">
-                  <div className="contact-content">
-                    <h2 className="text-[40px] font-semibold">
-                      Guest information
-                    </h2>
-
-                    {guest?.isVerified === false ? (
-                      <div className="flex items-center">
-                        <MdError
-                          size={24}
-                          style={{ color: "red", marginRight: "8px" }}
-                        />
-                        This user is not verified
-                      </div>
-                    ) : (
-                      <div className="flex items-center">
-                        <VscVerifiedFilled
-                          size={24}
-                          style={{ color: "green", marginRight: "8px" }}
-                        />
-                        This user is already verified
-                      </div>
-                    )}
-
-                    <form className="contact-form-items">
-                      <div className="form-clt">
-                        <span>ID</span>
-                        <input
-                          type="text"
-                          name="id"
-                          id="id"
-                          defaultValue={guest?.id || ""}
-                          placeholder="Your Name"
-                          disabled
-                        />
-                      </div>
-                      <div className="form-clt">
-                        <span>Your name*</span>
-                        <input
-                          type="text"
-                          name="name"
-                          id="name"
-                          defaultValue={guest?.name || ""}
-                          placeholder="Your Name"
-                          disabled
-                        />
-                      </div>
-                      <div className="form-clt">
-                        <span>Your Email*</span>
-                        <input
-                          type="text"
-                          name="email"
-                          id="email"
-                          defaultValue={guest?.email || ""}
-                          placeholder="Your Email"
-                          disabled
-                        />
-                      </div>
-                      <div className="form-clt">
-                        <span>Your Phone*</span>
-                        <input
-                          type="text"
-                          name="phone"
-                          id="phone"
-                          defaultValue={guest?.phone || ""}
-                          placeholder="Your Phone"
-                          disabled
-                        />
-                      </div>
-                      <div className="form-clt">
-                        <span>Your City*</span>
-                        <input
-                          type="text"
-                          name="city"
-                          id="city"
-                          defaultValue={guest?.city || ""}
-                          placeholder="Your City"
-                          disabled
-                        />
-                      </div>
-                    </form>
-                  </div>
-                </div>
-
-                {/* create a new user account */}
-
-                <div className="col-lg-6">
-                  <div className="contact-content">
-                    {guest?.isVerified === false ? (
-                      <AddUser
-                        guestId={guest?.id}
-                        guestEmail={guest?.email ?? undefined}
-                        guestType={guest?.type}
-                      />
-                    ) : null}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-    </>
-  );
+  return <>{guest ? <UserInfo guest={guest} /> : <div>Guest not found</div>}</>;
 }
