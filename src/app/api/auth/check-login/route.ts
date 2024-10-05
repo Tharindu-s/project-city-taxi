fb3b3052a5b94e563059235386db24c542669c5e
Tharindu Sandaruwan <sdtharindusandaruwan123@gmail.com>
2024-10-05 15:09:11 +0530

    v 0.1

--- src/app/api/auth/check-login/route.ts
+++ src/app/api/auth/check-login/route.ts
@@ -1,30 +1,29 @@
 import { getUserByEmail } from "@/lib/auth";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export async function GET() {
-    const token = cookies().get('token')?.value;
+  const token = cookies().get("token")?.value;
 
-    if (!token) {
-        return NextResponse.json(null); 
-    }
+  if (!token) {
+    return NextResponse.json(null);
+  }
 
-    // Verify and decode the JWT token
-    let decodedToken;
-    try {
-        decodedToken = jwt.verify(token, JWT_SECRET ?? ""); // Verify the token using the secret key
-    } catch (error) {
-        console.error('Invalid token:', error);
-        
-    }
+  // Verify and decode the JWT token
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, JWT_SECRET ?? ""); // Verify the token using the secret key
+  } catch (error) {
+    console.error("Invalid token:", error);
+  }
 
-    // Extract the email and type from the decoded token
-    const { email, type } = decodedToken as { email: string, type: string };
+  // Extract the email and type from the decoded token
+  const { email, type } = decodedToken as { email: string; type: string };
 
-    // Verify the token by fetching the user
-    const user = await getUserByEmail(email);
-    return NextResponse.json(user); // Return user details if logged in
+  // Verify the token by fetching the user
+  const user = await getUserByEmail(email);
+  return NextResponse.json(user); // Return user details if logged in
 }
